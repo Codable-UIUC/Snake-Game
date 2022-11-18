5854dc3685078bc6157516402aad9ff606042f27
Jiho Cheon <jihobang0906@gmail.com>
2022-11-18 08:27:28 -0600

    1

--- snake_game.py
+++ snake_game.py
@@ -25,3 +25,19 @@ class Snake_Game():
 
 game = Snake_Game()
 game.init_window()
+
+
+
+
+def draw_single_block(snake_block,color):
+    vertical_position = snake_block[0] * 15
+    horizontal_position = snake_block[1] * 15
+    pygame.draw.rect(dis, color, (vertical_position, horizontal_position, 15, 15)) 
+draw_single_block([x1,y1],(0, 255, 0))
+snake_list = [[200,203], [200,202], [200,201], [200,200]]
+def draw_snake(snake_list):
+    for snake_block in snake_list:
+        draw_single_block(snake_block,green)
+snake_list.append([x1,y1])
+if(len(snake_list)>=4):
+	del snake_list[0]
